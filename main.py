65b23700ce4aea8d0c9fef4e73823bcce8a9cb58
quickstark <dirk.nielsen@datadoghq.com>
2024-08-06 14:40:40 -0500

    Update error logic

--- main.py
+++ main.py
@@ -117,9 +117,9 @@ async def add_photo(file: UploadFile, backend: str = "mongo"):
                 error_message = f"Image Text Error - {' '.join(amztext)}"
                 raise CustomError(error_message)
         except CustomError as e:
-                # Handle the exception and report to Datadog
-                e.report_to_datadog()
-                return {"error": e.message}
+            # Handle the exception and report to Datadog
+            e.report_to_datadog()
+            return {"error": e.message}
 
     # Check if the image labels contained the word "bug" or "insect" and issue an error
     with tracer.trace("custom.error.bug"):
@@ -128,9 +128,9 @@ async def add_photo(file: UploadFile, backend: str = "mongo"):
                 error_message = f"Image Label Error - {' '.join(amzlabels)}"
                 raise CustomError(error_message)
         except CustomError as e:
-                # Handle the exception and report to Datadog
-                e.report_to_datadog()
-                return {"error": e.message}
+            # Handle the exception and report to Datadog
+            e.report_to_datadog()
+            return {"error": e.message}
 
     if backend == "mongo":
         # Attempt to upload the image to MongoDB
